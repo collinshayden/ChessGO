26da817ca588de88ab837777030545dfa7f3e117
jabirmin <jamiebirmingham3253@gmail.com>
2024-09-26 10:03:09 -0400

    Initial Commit

---
+++ ChessUI/ChessUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessUIApp.swift
+//  ChessUI
+//
+//  Created by James Birmingham on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ChessUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChessUI
+//
+//  Created by James Birmingham on 9/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
