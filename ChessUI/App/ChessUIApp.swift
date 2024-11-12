cb9a7c177eea7cc2478a87894fb8a35a888c4a6e
rohbot25 <ewrohan@gmail.com>
2024-11-12 10:08:00 -0500

    minor app changes, shouldn't change anything

--- ChessUI/App/ChessUIApp.swift
+++ ChessUI/App/ChessUIApp.swift
@@ -8,22 +8,13 @@
 import SwiftUI
 import FirebaseCore
 
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    // Uncomment the following line to clear UserDefaults every launch (for debugging purposes only)
-    UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
-    FirebaseApp.configure()
-
-    return true
-  }
-}
-
 @main
 struct ChessUIApp: App {
     
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    init() {
+        
+        FirebaseApp.configure()
+    }
     
     var body: some Scene {
         WindowGroup {
