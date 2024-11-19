4d8298d2df698d784f2e12c999508dac9d101267
rohbot25 <ewrohan@gmail.com>
2024-11-19 10:49:11 -0500

    sub view styling

--- ChessUI/SubViews/ProfileView.swift
+++ ChessUI/SubViews/ProfileView.swift
@@ -15,49 +15,50 @@ struct ProfileView : View {
     
     
     var body : some View {
-        Text("Profile").font(.largeTitle).padding(40)
-        Spacer()
-
-        HStack{
+        VStack{
+            Text("Profile").font(.largeTitle).padding(40)
             Spacer()
-            ZStack{
-                Button( action: {
-                    profile.pieceChoice -= 1
-                }, label: {
-                    Image(systemName: "arrow.left").imageScale(.large).foregroundColor(profile.pieceChoice != 0 ? .black : .gray).opacity(profile.pieceChoice != 0 ? 1 : 0.2)
-                }).disabled(profile.pieceChoice == 0)
-            }
+            
+            HStack{
+                Spacer()
+                ZStack{
+                    Button( action: {
+                        profile.pieceChoice -= 1
+                    }, label: {
+                        Image(systemName: "arrow.left").imageScale(.large).foregroundColor(profile.pieceChoice != 0 ? .white : .gray).opacity(profile.pieceChoice != 0 ? 1 : 0.2)
+                    }).disabled(profile.pieceChoice == 0)
+                }
+                Spacer()
+                ZStack{
+                    profile.pieces[profile.pieceChoice]
+                }.background(colors.lightGreen).cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                Spacer()
+                ZStack{
+                    Button( action: {
+                        profile.pieceChoice += 1
+                    }, label: {
+                        Image(systemName: "arrow.right").imageScale(.large).foregroundColor(.white).foregroundColor(profile.pieceChoice != profile.pieces.count - 1 ? .black : .gray).opacity(profile.pieceChoice != profile.pieces.count - 1 ? 1 : 0.2)
+                    }).disabled(profile.pieceChoice == profile.pieces.count - 1)
+                }
+                Spacer()
+                
+            }.padding()
+            Text("Choose your character")
             Spacer()
-            ZStack{
-                profile.pieces[profile.pieceChoice]
-            }.background(colors.lightGreen).cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
             Spacer()
-            ZStack{
-                Button( action: {
-                    profile.pieceChoice += 1
-                }, label: {
-                    Image(systemName: "arrow.right").imageScale(.large).foregroundColor(.black).foregroundColor(profile.pieceChoice != profile.pieces.count - 1 ? .black : .gray).opacity(profile.pieceChoice != profile.pieces.count - 1 ? 1 : 0.2)
-                }).disabled(profile.pieceChoice == profile.pieces.count - 1)
-            }
-            Spacer()
-
-        }.padding()
-        Text("Choose your character")
-        Spacer()
-        Spacer()
-        Button(action: {
-            withAnimation {
-                showProfile.toggle()
-                showMap.toggle()
-            }
-        }) {
-            Text("Back to Map")
-                .font(.headline)
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(10)
-        }
+            DefaultButtonView(buttonImage: "xmark", action: { showProfile = false ; showMap = true})
+        }.padding(20)
+            .environment(\.font, .custom("League Spartan", size: 32))
+            .foregroundColor(.white)
+            .background(LinearGradient(
+                gradient: Gradient(colors: [
+                    colors.darkGreen,
+                    colors.vermontGreen,
+                    colors.lightGreen
+                ]),
+                startPoint: .top,
+                endPoint: .bottom
+            ))
     }
 }
 
